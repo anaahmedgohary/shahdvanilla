9e4bbd069f1a883f8542a06d3a05cb2d627a3b4d
anaahmedgohary <gogoahmed13@gmail.com>
2022-09-29 19:26:26 +0200

    update styling

--- Sandwiches.js
+++ Sandwiches.js
@@ -1,78 +1,78 @@
-if(document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready()
+  ready();
 }
 
 function ready() {
+  // get back cart content before refresh page
+  let cartRowsBody = document.getElementById("cart-Rows");
+  let savedcartBody = JSON.parse(localStorage.getItem("mycart"));
+  cartRowsBody.innerHTML = savedcartBody;
+  updateTotalPrice();
+  cartCounter();
 
-    // get back cart content before refresh page
-    let cartRowsBody = document.getElementById("cart-Rows")
-    let savedcartBody = JSON.parse(localStorage.getItem('mycart'))
-    cartRowsBody.innerHTML = savedcartBody
-    updateTotalPrice()
-    cartCounter()
+  let imgs = document.getElementsByClassName("myImg");
+  for (let i = 0; i < imgs.length; i++) {
+    let img = imgs[i];
+    img.addEventListener("click", showPopup);
+  }
 
-    let imgs = document.getElementsByClassName('myImg');
-    for (let i = 0; i < imgs.length; i++) {
-        let img = imgs[i];
-        img.addEventListener('click', showPopup);
-    }
+  let span = document.getElementsByClassName("close")[0];
+  span.addEventListener("click", closePopup);
 
-    let span = document.getElementsByClassName('close')[0];
-    span.addEventListener('click', closePopup)
+  let addToCart = document.getElementsByClassName("add-to-btn");
+  for (let i = 0; i < addToCart.length; i++) {
+    let button = addToCart[i];
+    button.addEventListener("click", addsToCart);
+  }
 
-    let addToCart = document.getElementsByClassName("add-to-btn")
-    for (let i=0; i<addToCart.length; i++) {
-        let button = addToCart[i]
-        button.addEventListener("click", addsToCart)
-    }
+  let removeButton = document.getElementsByClassName("cart-rmov-btn");
+  for (let i = 0; i < removeButton.length; i++) {
+    let button = removeButton[i];
+    button.addEventListener("click", returnCartItem);
+  }
 
-    let removeButton = document.getElementsByClassName("cart-rmov-btn")
-    for (let i=0; i<removeButton.length; i++) {
-        let button = removeButton[i]
-        button.addEventListener("click", returnCartItem)
-    }
-
-    // checkout button function
-    let checkoutButton = document.getElementById("checkoutbtn")
-    checkoutButton.addEventListener("click", clearCart)
+  // checkout button function
+  let checkoutButton = document.getElementById("checkoutbtn");
+  checkoutButton.addEventListener("click", clearCart);
 }
 
 function showPopup(event) {
-    let imgClicked = event.target
-    let imagesContainer = imgClicked.parentElement
-    let images = imagesContainer.getElementsByClassName('myImg')[0]
-    console.log(images)
-    let popupSrc = document.getElementById('img01')
-    popupSrc.src = images.src
-    let imgDesc = document.getElementById('caption')
-    imgDesc.innerHTML = images.alt
-    let popElement = document.getElementById('myModal')
-    popElement.style.display = "block"
+  let imgClicked = event.target;
+  let imagesContainer = imgClicked.parentElement;
+  let images = imagesContainer.getElementsByClassName("myImg")[0];
+  console.log(images);
+  let popupSrc = document.getElementById("img01");
+  popupSrc.src = images.src;
+  let imgDesc = document.getElementById("caption");
+  imgDesc.innerHTML = images.alt;
+  let popElement = document.getElementById("myModal");
+  popElement.style.display = "block";
 }
 
 function closePopup(event) {
-    let modal = document.getElementById('myModal')
-    modal.style.display = "none"
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
 
 function addsToCart(event) {
-    let buttonClicked = event.target
-    let product = buttonClicked.parentElement.parentElement
-    let productImage = product.getElementsByClassName('gb-image')[0].src
-    let productName = product.getElementsByClassName('product-name')[0].innerText
-    let productPrice = product.getElementsByClassName('product-price')[0].innerText
-    console.log(productImage, productName, productPrice)
-    whatToAdd(productImage, productName, productPrice)
-    updateTotalPrice()
+  let buttonClicked = event.target;
+  let product = buttonClicked.parentElement.parentElement;
+  let productImage = product.getElementsByClassName("gb-image")[0].src;
+  let productName = product.getElementsByClassName("product-name")[0].innerText;
+  let productPrice =
+    product.getElementsByClassName("product-price")[0].innerText;
+  console.log(productImage, productName, productPrice);
+  whatToAdd(productImage, productName, productPrice);
+  updateTotalPrice();
 }
 
-function whatToAdd (a, b, c) {
-    let cartRow = document.createElement("div")
-    cartRow.classList.add("cart-row")
-    // (a, b, c) is just random parameters, it could be anything else like (whatever, ere, noi)
-    let cartRowContent = `
+function whatToAdd(a, b, c) {
+  let cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  // (a, b, c) is just random parameters, it could be anything else like (whatever, ere, noi)
+  let cartRowContent = `
     <div class="cart-column cart-Image">
         <img class="cart-item-img" src="${a}">
     </div>
@@ -80,58 +80,64 @@ function whatToAdd (a, b, c) {
         <span class="cart-item-name">${b}</span>
     </div>
     <div class="cart-column cart-qty">
-        <input class="cart-item-qty" type="number" min="1" step="1" />
+        <div>
+        <button class="addBtn btn btn-success">+</button>
+        <button class="subtractBtn btn btn-warning">-</button>
+        </div>
+        <p class="cart-item-qty">1</p>
     </div>
     <div class="cart-column cart-Price">
         <span class="cart-item-price">${c}</span>
     </div>
     <div class="cart-column cart-returnBtn">
-        <button class="cart-rmov-btn" type="button">Return</button>
-    </div>`
-    
-    cartRow.innerHTML = cartRowContent
+        <button class="cart-rmov-btn btn btn-danger" type="button">Return</button>
+    </div>`;
 
-    let cartBody = document.getElementById("cart-Rows")
-    cartBody.append(cartRow)
-    //update & store that new row to session storage
-    let newcartBody = cartBody.innerHTML
-    localStorage.setItem('mycart', JSON.stringify(newcartBody))
-    
-    // update Return (remove) button for newly added to cart elements 
-    cartRow.getElementsByClassName("cart-rmov-btn")[0].addEventListener("click", returnCartItem)
+  cartRow.innerHTML = cartRowContent;
 
-    cartCounter()
+  let cartBody = document.getElementById("cart-Rows");
+  cartBody.append(cartRow);
+  //update & store that new row to session storage
+  let newcartBody = cartBody.innerHTML;
+  localStorage.setItem("mycart", JSON.stringify(newcartBody));
+
+  // update Return (remove) button for newly added to cart elements
+  cartRow
+    .getElementsByClassName("cart-rmov-btn")[0]
+    .addEventListener("click", returnCartItem);
+
+  cartCounter();
 }
 
 function returnCartItem(event) {
-    let buttonclicked = event.target
-    let rowToremove = buttonclicked.parentElement.parentElement
-    rowToremove.remove()
-    updateTotalPrice()
-    
-    //update & store left over rows to session storage
-    let cartBody = document.getElementById("cart-Rows")
-    let newcartBody = cartBody.innerHTML
-    localStorage.setItem('mycart', JSON.stringify(newcartBody))
+  let buttonclicked = event.target;
+  let rowToremove = buttonclicked.parentElement.parentElement;
+  rowToremove.remove();
+  updateTotalPrice();
+
+  //update & store left over rows to session storage
+  let cartBody = document.getElementById("cart-Rows");
+  let newcartBody = cartBody.innerHTML;
+  localStorage.setItem("mycart", JSON.stringify(newcartBody));
 }
 
 const updateTotalPrice = () => {
-    let cartContainer = document.getElementById("cart") //for good practise purposes only
-    let cartRows = cartContainer.getElementsByClassName("cart-row")
-    let total = 0
-    for (let i=0; i<cartRows.length; i++) {
-        let cartRow = cartRows[i]
-        let itemPrice = cartRow.getElementsByClassName("cart-item-price")[0]
-        let itemPriceValue = parseFloat(itemPrice.innerText)
-        total += itemPriceValue
-    }
+  let cartContainer = document.getElementById("cart"); //for good practise purposes only
+  let cartRows = cartContainer.getElementsByClassName("cart-row");
+  let total = 0;
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i];
+    let itemPrice = cartRow.getElementsByClassName("cart-item-price")[0];
+    let itemPriceValue = parseFloat(itemPrice.innerText);
+    total += itemPriceValue;
+  }
 
-    document.getElementById("TotalValue").innerText = total
+  document.getElementById("TotalValue").innerText = total;
 
-    // storing paypal total to localStorage
-    /* let paypalTotal = total
+  // storing paypal total to localStorage
+  /* let paypalTotal = total
     localStorage.setItem("cartTotal", JSON.stringify(paypalTotal)) */
-}
+};
 
 /* function updateTotalPrice3 ()  {
     let totalElement = document.getElementById("TotalValue")
@@ -148,32 +154,27 @@ const updateTotalPrice = () => {
 } */
 
 function clearCart() {
-    
-    //let cartRows = document.getElementsById("cart")
-    let cartRoww = document.getElementById("cart-Rows")
-    while(cartRoww.hasChildNodes()) {
-        cartRoww.removeChild (cartRoww.firstChild)
-    }
+  //let cartRows = document.getElementsById("cart")
+  let cartRoww = document.getElementById("cart-Rows");
+  while (cartRoww.hasChildNodes()) {
+    cartRoww.removeChild(cartRoww.firstChild);
+  }
 
-    //update & store  session storage values
-    let cartBody = document.getElementById("cart-Rows")
-    let newcartBody = cartBody.innerHTML
-    localStorage.setItem('mycart', JSON.stringify(newcartBody))
+  //update & store  session storage values
+  let cartBody = document.getElementById("cart-Rows");
+  let newcartBody = cartBody.innerHTML;
+  localStorage.setItem("mycart", JSON.stringify(newcartBody));
 
-    updateTotalPrice()
+  updateTotalPrice();
 }
 
 // counter for items in cart
 function cartCounter() {
-    let cartCounter = document.getElementById("cartCounter")
-    let itemsInCart = 0
-    let cartrows = document.getElementsByClassName("cart-row")
-    itemsInCart = cartrows.length
-    console.log(itemsInCart)
-    
+  let cartCounter = document.getElementById("cartCounter");
+  let itemsInCart = 0;
+  let cartrows = document.getElementsByClassName("cart-row");
+  itemsInCart = cartrows.length;
+  console.log(itemsInCart);
 
-
-
-
-    cartCounter.innerText = itemsInCart
+  cartCounter.innerText = itemsInCart;
 }
--- cart.js
+++ cart.js
@@ -1,221 +1,258 @@
-if(document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready()
+  ready();
 }
 
 function ready() {
+  // get back cart content before refresh page
+  let cartRowsBody = document.getElementById("cart-Rows");
+  let savedcartBody = JSON.parse(localStorage.getItem("mycart"));
+  cartRowsBody.innerHTML = savedcartBody;
+  sandwitchobj();
+  updateTotalPrice();
 
-    // get back cart content before refresh page
-    let cartRowsBody = document.getElementById("cart-Rows");
-    let savedcartBody = JSON.parse(localStorage.getItem('mycart'));
-    cartRowsBody.innerHTML = savedcartBody;
-    sandwitchobj();
-    updateTotalPrice()
-    
-    // Quantity change listener
-    //let caartroow = document.getElementsByClassName("cart-row")
-    let qtyEle = document.getElementsByClassName("cart-item-qty")
-    for(let i=0; i<qtyEle.length; i++) {
-        everyqtyele = qtyEle[i]
-        everyqtyele.addEventListener("change", updateOrderQty)
-    }
-    
+  // Quantity change listener
+  //let caartroow = document.getElementsByClassName("cart-row")
+  let qtyEle = document.getElementsByClassName("cart-item-qty");
+  for (let i = 0; i < qtyEle.length; i++) {
+    everyqtyele = qtyEle[i];
+    everyqtyele.addEventListener("click", updateOrderQty);
+  }
 
-    // return button listener
-    let removeButton = document.getElementsByClassName("cart-rmov-btn")
-    for (let i=0; i<removeButton.length; i++) {
-        let button = removeButton[i]
-        button.addEventListener("click", returnCartItem)
-    }
+  //Quantity add & subtract
+  let addbtn = document.getElementsByClassName("addBtn");
+  let subtractBtn = document.getElementsByClassName("subtractBtn");
+  for (let i = 0; i < addbtn.length; i++) {
+    let button = addbtn[i];
+    button.addEventListener("click", quantityPlus1);
+  }
 
-    /* // checkout button function
+  for (let i = 0; i < subtractBtn.length; i++) {
+    let button = subtractBtn[i];
+    button.addEventListener("click", quantityMinus1);
+  }
+
+  // return button listener
+  let removeButton = document.getElementsByClassName("cart-rmov-btn");
+  for (let i = 0; i < removeButton.length; i++) {
+    let button = removeButton[i];
+    button.addEventListener("click", returnCartItem);
+  }
+
+  /* // checkout button function
     let checkoutButton = document.getElementById("checkoutbtn")
     checkoutButton.addEventListener("click", clearCart)
  */
-    // Generate order id
-    let generateBtn = document.getElementById("generateOId")
-    generateBtn.addEventListener("click", orderIdGenerator)
-    document.getElementById("generateOId").click();
+  // Generate order id
+  let generateBtn = document.getElementById("generateOId");
+  generateBtn.addEventListener("click", orderIdGenerator);
+  document.getElementById("generateOId").click();
 
-    // Actions: (show chose payment button) after form submit
-    let cusform = document.getElementById("customerForm")
-    cusform.addEventListener("submit", afterFormSubmit)
-  
+  // Actions: (show chose payment button) after form submit
+  let cusform = document.getElementById("customerForm");
+  cusform.addEventListener("submit", afterFormSubmit);
 }
 
+function quantityPlus1(event) {
+  let clickin = event.target;
+  let quantDiv = clickin.parentElement.parentElement;
 
-
-function afterFormSubmit(event){
-
-    // store form in local storage
-
-    // show payment methods button
-    let showBtn = document.getElementById("chosePayment")
-    showBtn.style.display = "none"
-
-    // show payment methods
-    paypalButns();
-
-    // hide cart rows & update total price
-    let cartBoty = document.getElementById("cart-Rows")
-    cartBoty.style.display="none"
-    updateTotalPrice();
+  let quantP = quantDiv.getElementsByClassName("cart-item-qty")[0];
+  //   quantP.innerText = 1;
+  //console.log(quantP.innerHTML);
+  quantP.innerText = parseInt(quantP.innerText) + 1;
+  quantP.click();
+  updateTotalPrice();
 }
 
+function quantityMinus1(event) {
+  let clickin = event.target;
+  let quantDiv = clickin.parentElement.parentElement;
 
+  let quantP = quantDiv.getElementsByClassName("cart-item-qty")[0];
+  //   quantP.innerText = 1;
+  //console.log(quantP.innerHTML);
+  quantP.innerText = parseInt(quantP.innerText) - 1;
+  quantP.click();
+  updateTotalPrice();
+}
+
+function afterFormSubmit(event) {
+  // store form in local storage
+
+  // show payment methods button
+  // let showBtn = document.getElementById("chosePayment");
+  // showBtn.style.display = "none";
+
+  // show payment methods
+  paypalButns();
+
+  // hide cart rows & update total price
+  let cartBoty = document.getElementById("cart-Rows");
+  cartBoty.style.display = "none";
+  updateTotalPrice();
+}
 
 function returnCartItem(event) {
-    let buttonclicked = event.target
-    let rowToremove = buttonclicked.parentElement.parentElement
-    rowToremove.remove()
-    updateTotalPrice()
-    
-    //update & store left over rows to session storage
-    let cartBody = document.getElementById("cart-Rows")
-    let newcartBody = cartBody.innerHTML
-    localStorage.setItem('mycart', JSON.stringify(newcartBody))
-    window.location.reload();
+  let buttonclicked = event.target;
+  let rowToremove = buttonclicked.parentElement.parentElement;
+  rowToremove.remove();
+  updateTotalPrice();
+
+  //update & store left over rows to session storage
+  let cartBody = document.getElementById("cart-Rows");
+  let newcartBody = cartBody.innerHTML;
+  localStorage.setItem("mycart", JSON.stringify(newcartBody));
+  window.location.reload();
 }
 
-function updateOrderQty(event){
-    let qtychange = event.target
-    if(isNaN(qtychange.value) || qtychange.value<=0) {
-        qtychange.value = 1
-    }
-    updateTotalPrice()
+function updateOrderQty(event) {
+  let qtychange0 = event.target;
+  let qtychange = parseInt(qtychange0.innerText);
+  console.log("what");
+  if (isNaN(qtychange) || qtychange <= 0) {
+    qtychange0.innerText = 1;
+  }
+  updateTotalPrice();
 }
 
-function updateTotalPrice()
-{
-    let cartContainer = document.getElementById("cart") //for good practise purposes only
-    let cartRows = cartContainer.getElementsByClassName("cart-row")
-    let total = 0
-    for (let i = 0; i < cartRows.length; i++) {
-        let cartRow = cartRows[i]
-        let itemPrice = cartRow.getElementsByClassName("cart-item-price")[0]
-        let itemPriceValue = parseFloat(itemPrice.innerText)
-        let qty = cartRow.getElementsByClassName("cart-item-qty")[0].value
-        total += (itemPriceValue * qty)
-    }
+function updateTotalPrice() {
+  let cartContainer = document.getElementById("cart"); //for good practise purposes only
+  let cartRows = cartContainer.getElementsByClassName("cart-row");
+  let total = 0;
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i];
+    let itemPrice = cartRow.getElementsByClassName("cart-item-price")[0];
+    let itemPriceValue = parseFloat(itemPrice.innerText);
+    let qty = cartRow.getElementsByClassName("cart-item-qty")[0].innerText;
+    total += itemPriceValue * parseInt(qty);
+  }
 
-    document.getElementById("TotalValue").innerText = total;
-    //$("#orderTotal").val(total)
-    document.getElementById("orderTotal").value = total;
-    // Transfer Cart Total to Payment method Paypal
-    /* let valueofPPpayment = total */
-    //localStorage.setItem('mycart', JSON.stringify(document.getElementById("cart-Rows").innerHTML))}
-    let cartBody = document.getElementById("cart-Rows")
-    let newcartBody = cartBody.innerHTML
-    localStorage.setItem('mycart', JSON.stringify(newcartBody))
+  document.getElementById("TotalValue").innerText = total;
+  //$("#orderTotal").val(total)
+  //document.getElementById("orderTotal").value = total;
+  // Transfer Cart Total to Payment method Paypal
+  /* let valueofPPpayment = total */
+  //localStorage.setItem('mycart', JSON.stringify(document.getElementById("cart-Rows").innerHTML))}
+  let cartBody = document.getElementById("cart-Rows");
+  let newcartBody = cartBody.innerHTML;
+  localStorage.setItem("mycart", JSON.stringify(newcartBody));
 }
-
 
 function clearCart() {
-    
-    //let cartRows = document.getElementsById("cart")
-    let cartRoww = document.getElementById("cart-Rows")
-    while(cartRoww.hasChildNodes()) {
-        cartRoww.removeChild (cartRoww.firstChild)
-    }
+  //let cartRows = document.getElementsById("cart")
+  let cartRoww = document.getElementById("cart-Rows");
+  while (cartRoww.hasChildNodes()) {
+    cartRoww.removeChild(cartRoww.firstChild);
+  }
 
-    //update local storage values
-    let cartBody2 = document.getElementById("cart-Rows")
-    let newcartBody2 = cartBody.innerHTML
-    localStorage.removeItem('mycart')
+  //update local storage values
+  let cartBody2 = document.getElementById("cart-Rows");
+  let newcartBody2 = cartBody.innerHTML;
+  localStorage.removeItem("mycart");
 
-    updateTotalPrice()
+  updateTotalPrice();
 }
 
 function orderIdGenerator(event) {
-    
-    let orderIdInput = document.getElementById("orderIdInput");
-    let orderval = document.getElementById("OrderID")
-    
-    // create unique order id
-    let rText = ""
-    let alphaBets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    rText += alphaBets.charAt(Math.floor(Math.random() * alphaBets.length)) + alphaBets.charAt(Math.floor(Math.random() * alphaBets.length))
-    orderval.value = rText + " " + Math.floor(Math.random() * 10000);
+  let orderIdInput = document.getElementById("orderIdInput");
+  let orderval = document.getElementById("OrderID");
 
-    //updateTotalPrice();
-    // for fill order id in form
-    let geneIdBtn = document.getElementById("generateOId");
-    geneIdBtn.style.pointerEvents="none"
-    geneIdBtn.style.display="none";
+  // create unique order id
+  let rText = "";
+  let alphaBets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  rText +=
+    alphaBets.charAt(Math.floor(Math.random() * alphaBets.length)) +
+    alphaBets.charAt(Math.floor(Math.random() * alphaBets.length));
+  orderval.value = rText + " " + Math.floor(Math.random() * 10000);
+
+  //updateTotalPrice();
+  // for fill order id in form
+  let geneIdBtn = document.getElementById("generateOId");
+  geneIdBtn.style.pointerEvents = "none";
+  geneIdBtn.style.display = "none";
 }
 
 function sandwitchobj() {
-    let sandwitches = {
-        tameia:10,
-        romicheese:15,
-        shawerma:25,
-        gbeef:30
+  let sandwitches = {
+    tameia: 10,
+    romicheese: 15,
+    shawerma: 25,
+    gbeef: 30,
+  };
+  /* console.log(sandwitches.gbeef) */
+  let pname = document.getElementsByClassName("cart-item-name");
+
+  for (let i = 0; i < pname.length; i++) {
+    let productName = pname[i].innerText;
+
+    /* console.log(productName) */
+    if (productName == "طعمية") {
+      let cartroww = pname[i].parentElement.parentElement;
+      let tameaprice = sandwitches.tameia;
+      /* console.log(tameaprice) */
+      cartroww.getElementsByClassName("cart-item-price")[0].innerText =
+        tameaprice;
+    } else if (productName == "جبنة رومي") {
+      let cartroww = pname[i].parentElement.parentElement;
+      let romiprice = sandwitches.romicheese;
+      cartroww.getElementsByClassName("cart-item-price")[0].innerText =
+        romiprice;
+    } else if (productName == "شاورما") {
+      let cartroww = pname[i].parentElement.parentElement;
+      let shawermaprice = sandwitches.shawerma;
+      cartroww.getElementsByClassName("cart-item-price")[0].innerText =
+        shawermaprice;
+    } else if (productName == "لحمة مفرومة") {
+      let cartroww = pname[i].parentElement.parentElement;
+      let gbeefprice = sandwitches.gbeef;
+      cartroww.getElementsByClassName("cart-item-price")[0].innerText =
+        gbeefprice;
+    } else {
+      alert("This item is not in sandwitchobj");
     }
-    /* console.log(sandwitches.gbeef) */
-    let pname = document.getElementsByClassName("cart-item-name")
-    
-    for(let i=0; i < pname.length; i++) {
-        
-        let productName = pname[i].innerText
-        
-        /* console.log(productName) */
-        if(productName == "طعمية"){
-            let cartroww = pname[i].parentElement.parentElement;
-            let tameaprice = sandwitches.tameia;
-            /* console.log(tameaprice) */
-            cartroww.getElementsByClassName("cart-item-price")[0].innerText = tameaprice;
-
-        } else if(productName == "جبنة رومي"){
-            let cartroww = pname[i].parentElement.parentElement;
-            let romiprice = sandwitches.romicheese;
-            cartroww.getElementsByClassName("cart-item-price")[0].innerText = romiprice;
-
-        } else if(productName == "شاورما"){
-            let cartroww = pname[i].parentElement.parentElement;
-            let shawermaprice = sandwitches.shawerma;
-            cartroww.getElementsByClassName("cart-item-price")[0].innerText = shawermaprice;
-
-        } else if(productName == "لحمة مفرومة"){
-            let cartroww = pname[i].parentElement.parentElement;
-            let gbeefprice = sandwitches.gbeef;
-            cartroww.getElementsByClassName("cart-item-price")[0].innerText = gbeefprice;
-        } else {
-            alert("This item is not in sandwitchobj");
-        }
-    }
+  }
 }
 
 function paypalButns() {
+  valueofPPpayment = document.getElementById("TotalValue").innerText;
+  console.log(valueofPPpayment);
 
-    valueofPPpayment = document.getElementById("TotalValue").innerText
-    console.log(valueofPPpayment)
-
-
-    paypal.Buttons({
-        // Sets up the transaction when a payment button is clicked
-        createOrder: (data, actions) => {
+  paypal
+    .Buttons({
+      // Sets up the transaction when a payment button is clicked
+      createOrder: (data, actions) => {
         return actions.order.create({
-            purchase_units: [{
-            amount: {
-                value: valueofPPpayment // Can also reference a variable or function
-            }
-            }]
+          purchase_units: [
+            {
+              amount: {
+                value: valueofPPpayment, // Can also reference a variable or function
+              },
+            },
+          ],
         });
-        },
-        // Finalize the transaction after payer approval
-        onApprove: (data, actions) => {
-        return actions.order.capture().then(function(orderData) {
-            // Successful capture! For dev/demo purposes:
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-            const transaction = orderData.purchase_units[0].payments.captures[0];
-            alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
-            // When ready to go live, remove the alert and show a success message within this page. For example:
-            // const element = document.getElementById('paypal-button-container');
-            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-            // Or go to another URL:  actions.redirect('thank_you.html');
+      },
+      // Finalize the transaction after payer approval
+      onApprove: (data, actions) => {
+        return actions.order.capture().then(function (orderData) {
+          // Successful capture! For dev/demo purposes:
+          console.log(
+            "Capture result",
+            orderData,
+            JSON.stringify(orderData, null, 2)
+          );
+          const transaction = orderData.purchase_units[0].payments.captures[0];
+          alert(
+            `Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`
+          );
+          // When ready to go live, remove the alert and show a success message within this page. For example:
+          // const element = document.getElementById('paypal-button-container');
+          // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+          // Or go to another URL:  actions.redirect('thank_you.html');
+          $("#transaction-id").text(`${transaction.id}`);
         });
-        }
-    }).render('#paypalPayBtn');
+      },
+    })
+    .render("#paypalPayBtn");
 }
-
